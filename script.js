29da4117329105b383de71a4b9e8deac8de3f427
josene224 <tatijosene@gmail.com>
2025-05-16 10:03:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,4 @@ window.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('scrolled');
   }
-window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('header');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
 });
